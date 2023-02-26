ddf317c3ee0d3f2031af7f948979cac7ec1dfb27
Mirko Acevedo <mirko.acevedo.n@gmail.com>
2023-02-26 13:51:38 -0300

    Detalles de api

--- pokeapi/src/api/Poke.js
+++ pokeapi/src/api/Poke.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 
 
          const getPokemons = async (stat)=> {
-            const peticion = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1500').then((resp)=> resp.data.results);
+            const peticion = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1199').then((resp)=> resp.data.results);
+    
 
 
           
@@ -15,20 +16,12 @@ import axios from 'axios';
             
          }
 
-         const getImg = 
-            async (img)=>{
-
-                const img2 = await axios.get('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/');
-
-
-                const imgURL = img2+{id}+".png"
-                img(imgURL)
-            }
+    
 
         export { 
 
             getPokemons,
-            getImg
+          
         }
 
     
--- pokeapi/src/components/HomePages.jsx
+++ pokeapi/src/components/HomePages.jsx
@@ -1,53 +1,58 @@
 import React,{useEffect, useState} from 'react'
-import { getPokemons } from '../api/Poke';
+import {  getPokemons } from '../api/Poke';
 
 
 
 
 const HomePage = () => {
 
-    const pokemones = {
-        nombre: name
-    }
+
+
 
 const [pokemons, setPoke] = useState([]);
 
 
 
+
+
 useEffect(() => {
     getPokemons(setPoke);
+
     
 }, []);
+
+
 
 
     return (  
         <>
         
-            {pokemons.map((items,index)=>(<div key={index}
+            {pokemons.map((items,index)=>
+            (
+             
             
             
             
-            className="row"> 
+            <div key={index}
             
-            <div className='col-5'>
-                
-                
+            
+            
+            className="container-fluid"> 
+            <div className="row ">
+            <div className='col-md-3'>
                  <div className="card"> 
 
-                 <div className="card-img-top"> {}</div>
+                 <div className="card-img-top shadow-lg p-3 mb-5 bg-body-tertiary rounded"> 
+                 <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${items.url.split('/')[6]}.png`} 
+                 alt='Imagen no encontrada' /> </div>
                  
                 <div className="card-title">{items.name}</div>
-
-                
-
+                </div>
                  
-                 </div>
+            </div>
                  
                  
-                 
-                 
-        
-         </div></div>))}
+                 </div></div>))}
         
         </>
 
